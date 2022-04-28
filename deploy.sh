0a170d745eee2478b4c6b9ff08aa1fb1591137ab
Antonio Basic <antoniobasic@gmail.com>
2022-04-28 12:31:49 +0200

    Update git  origin for deployment

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/ExtensionEngine/tailor-docs.git master:gh-pages
+git push -f git@github.com:ExtensionEngine/tailor-docs.git master:gh-pages
 
 cd -
